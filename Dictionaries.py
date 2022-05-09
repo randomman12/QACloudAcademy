40cbc5e710ab28f4991d35025593740e4f412617
randomman12 <84386685+randomman12@users.noreply.github.com>
2022-05-09 15:15:57 +0100

    Update Dictionaries.py

--- Dictionaries.py
+++ Dictionaries.py
@@ -23,19 +23,26 @@ dict3 = {"brand": "ford", "model": "focus"}
 print(dict3)
 
 #CODE4: Create DICTIONARY with multiple and nested key-value pairs
-dict4 = {"brand": "ford", "model": "focus"}
+dict4 = {"brand": "ford", "model": "focus", "count": 100}
 print(dict4)
 #CODE5: Iterate over DICTIONARY with multiple and nested key-value pairs
 dict5 = {"brand": "ford", "model": "focus", "data": {"val1": 1, "val2": 2}}
 #CODE6: Search key in DICTIONARY
-
-
+print("brand" in dict4)
 #CODE7: Retrieve value from DICTIONARY by key
-
+item0 = dict4["brand"]
+item1 = dict4["model"]
+print(item0)
+print(item1)
 #CODE8: Change existing value in DICTIONARY
-
+dict4["brand"] = "lamborghini"
+print(dict4)
 #CODE9: Add new key-value pair to DICTIONARY
-
+dict4["engine"] = "petrol"
+print(dict4)
 #CODE10: Pop existing key-value pair from DICTIONARY
-
+test = dict4.pop("engine", None)
+print (test)
 #CODE11: Pop non-existing key-value pair from DICTIONARY
+test1 = dict4.pop("mpg", None)
+print (test1)
